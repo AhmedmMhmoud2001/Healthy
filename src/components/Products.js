d1891af62bd74def13e4fff036830f0e787c130e
AhmedmMhmoud2001 <ahmedmahmoud2152001@gmail.com>
2022-11-24 21:46:34 +0200

    add products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,23 +1,37 @@
 import React from 'react'
+import data from '../data'
+import {Link} from "react-router-dom";
 
 export const Products = () => {
+    // console.log(data.products)
   return (
-    <div>
-       <div className="card">
-          <div className="card-header">
-              <img src="" alt="" />
-          </div>
-          <div className="card-body">
-              <h3 className="title">product.title.substring(0, 15)..</h3>
-              <div className="other">
-                  <span>Rate: product.rating.rate</span>
-                  <span>$product.price</span>
-              </div>
-          </div>
-          <div className="card-footer">
-             
-          </div>
-      </div>
+    <div className='Products'>
+      <div  className='Productsfitter'>
+
+      </div> 
+      <div>
+        <div>
+          <h2>Products</h2>
+        </div>
+        <div className='ProductsProduct'>
+        {data.products.map((product) => (
+                <div className='Product' key={product.id}>
+                  <div>
+                    <img src={product.image} width="100%" alt={product.name}/>
+                  </div>
+                  <div>
+                    <h2 className='productname'>{product.name}</h2>
+                    <span>{product.category}</span>
+                    <span>Rating {product.rating}</span>
+                    <h2 className='productprice'>{product.price} LE</h2>
+                    <div>
+                      <Link to={`/Cart/${product.id}`} className='buyNow'>Buy Now</Link>
+                    </div>
+                  </div>
+                </div>
+          ))}
+        </div>
+      </div>    
     </div>
   )
 }
