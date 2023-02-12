9fda7eff290fae8f81c14ccf6d54639b43786e2c
AhmedmMhmoud2001 <ahmedmahmoud2152001@gmail.com>
2023-02-12 18:36:13 +0200

    change in design and add specialties and massenger and articles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
-import Header from './components/Header';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
-import Home from './components/Home';
- import About from './components/About';
-import Contact from './components/Contact';
-import Cart from './components/Cart';
-import  {Login}  from './components/Login';
-import  {Prescription} from './components/Prescription';
-import { Products } from './components/Products';
-import { Product } from './components/Product';
-import { Register } from './components/Register';
-import { Footer } from './components/Footer';
+// ============= components ==================
+import Header from './components/Header/Header';
+import { Product } from './components/Product/Product';
+import { Footer } from './components/Footer/Footer';
+// =============pages============
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Specialties from './pages/Specialties/Specialties';
+import Articles from './pages/Articles/Articles';
+import Messenger from './pages/Messenger/Messenger';
+import Cart from './pages/Cart/Cart';
+import { Register } from './pages/Register/Register';
+import { Products } from './pages/Products/Products';
+import  {Login}  from './pages/Login/Login';
 //import { Register } from './components/Register';
 function App() {
   return (
@@ -25,12 +29,14 @@ function App() {
           <Route path="/Home" element={<Home />}/>
           <Route path="/Products" element={<Products/>}/>
           <Route path="/Products/:id" element={<Product/>}/>
-          <Route path="/Prescription" element={<Prescription/>}/>
+          <Route path="/Specialties" element={<Specialties/>}/>
+          <Route path="/Articles" element={<Articles/>}/>
+          <Route path="/Messenger" element={<Messenger/>}/>
           <Route path="/About" element={<About/>}/>
           <Route path="/Cart" element={<Cart/>}/>
           <Route path="/Contact" element={<Contact />}/>
           <Route path="/Register" element={<Register/>}/>
-          <Route path="/Login" element={<Login />}/>  
+          <Route path="/Login" element={<Login/>}/>  
       </Routes>
       <Footer/>
     </Router>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,145 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus,faSignIn,faUserPlus,faPhone,faEnvelope,faBars,faBell} from '@fortawesome/free-solid-svg-icons'
+import { faInstagram ,faTwitter ,faFacebook ,faTelegram ,faFacebookMessenger } from '@fortawesome/free-brands-svg-icons'
+import {Link} from "react-router-dom";
+import "./header.css"
+import { useSelector } from 'react-redux';
+const Header = () => {
+const cart = useSelector((state) => state.cart)
+
+const slidbar = () => {
+  const element = document.getElementById("slidbar");
+
+  if(element.style.display==="none"){
+    element.style.display="block";
+  }
+  else{
+    element.style.display="none"
+  }
+}
+
+const getTotalQuantity = () => {
+  let total = 0
+  cart.forEach(item => {
+    total += item.quantity
+  })
+  return total
+}
+  return (
+    <>
+      <div className='headerheaderRight'>
+        <div> <Link to="">< FontAwesomeIcon icon={faPhone}/></Link></div>
+        <div> <Link to="">< FontAwesomeIcon icon={faEnvelope}/></Link></div>
+        <div> <Link to="">< FontAwesomeIcon icon={faTwitter}/></Link></div>
+        <div> <Link to="">< FontAwesomeIcon icon={faTelegram}/></Link></div>
+        <div> <Link to="">< FontAwesomeIcon icon={faInstagram}/></Link></div>
+        <div> <Link to="">< FontAwesomeIcon icon={faFacebook}/></Link></div>
+      </div>
+    <div className='header'>
+      
+      <div className='headerTitle'>
+          <p>Healthy</p>
+      </div>
+      <div className='headerIconSignandCart'>
+        <ul>
+        <li>
+            <Link to="/Login">
+            <FontAwesomeIcon className='iconheader' icon={faBell}></FontAwesomeIcon>
+            <p className='TotalNotifications'>0</p>
+              </Link>
+            </li>
+        <li>
+            <Link to="/Messenger">
+            <FontAwesomeIcon className='iconheader' icon={faFacebookMessenger}></FontAwesomeIcon>
+            <p className='TotalMessage'>0</p>
+              </Link>
+            </li>
+            <li className='cartcounter'>
+            <Link to="/Cart">
+              <FontAwesomeIcon className='iconheader' icon={faCartPlus}></FontAwesomeIcon>
+              <p className='TotalQuantity'>{getTotalQuantity() || 0}</p>
+              </Link>
+            </li>
+            <li>
+            <Link to="/Login">
+            <FontAwesomeIcon className='iconheader' icon={faSignIn}></FontAwesomeIcon>
+              Login
+              </Link>
+            </li>
+            <li>
+            <Link to="/Register">
+            <FontAwesomeIcon className='iconheader' icon={faUserPlus}></FontAwesomeIcon>
+              Register
+            </Link>
+            </li>
+        </ul>
+      </div>
+      <div className='barsIcon'>
+        <FontAwesomeIcon icon={faBars} onClick={slidbar}/>
+        <div className='slidbar' id="slidbar">
+        <ul>
+        <li>
+            <Link to="/Messenger">
+              <FontAwesomeIcon  icon={faBell}></FontAwesomeIcon>
+              Notifications
+              {/* <p >0</p> */}
+            </Link>
+        </li>
+        <li>
+            <Link to="/Messenger">
+            <FontAwesomeIcon icon={faFacebookMessenger}></FontAwesomeIcon>
+            Messenger
+            {/* <p>0</p> */}
+            </Link>
+          </li>
+          <li >
+            <Link to="/Cart">
+              <FontAwesomeIcon  icon={faCartPlus}></FontAwesomeIcon>
+              Cart
+              {/* <p >{getTotalQuantity() || 0}</p> */}
+            </Link>
+          </li>
+            <li>
+            <Link to="/Login">
+            <FontAwesomeIcon  icon={faSignIn}></FontAwesomeIcon>
+              Login
+              </Link>
+            </li>
+            <li>
+            <Link to="/Register">
+            <FontAwesomeIcon icon={faUserPlus}></FontAwesomeIcon>
+              Register
+            </Link>
+            </li>
+        </ul>
+      </div>
+      </div>
+    </div>
+    <div className='headerLink'>
+          <ul>
+            <li>
+            <Link to="/home">Home</Link>
+            </li>
+            <li>
+            <Link to="/About">About</Link>
+            </li>
+            <li>
+            <Link to="/Specialties">Specialties</Link>
+            </li>
+            <li>
+            <Link to="/Products">Products</Link>
+            </li>
+            <li>
+            <Link to="/Articles">Articles</Link>
+            </li>
+            <li>
+            <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+      </div>
+    </>
+  )
+}
+
+export default Header
